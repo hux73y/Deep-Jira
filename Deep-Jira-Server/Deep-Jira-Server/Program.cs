6fd100b5119a7a900773240a3d9955551a47a3e8
Percy Wünsch <bashx@web.de>
2020-05-11 22:29:46 +0200

    init

---
+++ Deep-Jira-Server/Deep-Jira-Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Deep_Jira_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
