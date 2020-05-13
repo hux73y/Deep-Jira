69b31dff8fe53609afdb206cb0ac7a3f379ee37b
Percy Wünsch <bashx@web.de>
2020-05-13 08:51:21 +0200

    classes added

--- Deep-Jira-Server/Deep-Jira-Server/Program.cs
+++ Deep-Jira-Server/Deep-Jira-Server/Program.cs
@@ -9,17 +9,12 @@ namespace Deep_Jira_Server
         {
             Thread t = new Thread(delegate ()
             {
-                //string ip = Console.ReadLine();
-                // replace the IP with your system IP Address...
-                //Server crowdServer = new Server("10.242.22.241", 5050);
-                string[] prefixes = { "http://192.168.178.39:80/start/" }; 
-                Server server = new Server(prefixes);
+                Server server = new Server("192.168.178.39", 80);
             });
             t.Start();
 
             Console.WriteLine("Server Started");
-            //HttpProcessor httpProcessor = new HttpProcessor();
-            //httpProcessor.WriteRequest();
+            
         }
     }
 }
--- Deep-Jira-Server/Deep-Jira-Server/Server.cs
+++ Deep-Jira-Server/Deep-Jira-Server/Server.cs
@@ -10,15 +10,13 @@ namespace Deep_Jira_Server
 {
     class Server
     {
-        HttpListener webServer = null;
+        TcpListener server = null;
 
-        public Server(string [] prefixes)
+        public Server(string ip, int port)
         {
-            webServer = new HttpListener();
-            foreach (string s in prefixes)
-                webServer.Prefixes.Add(s);
-
-            webServer.Start();
+            IPAddress localAddr = IPAddress.Parse(ip);
+            server = new TcpListener(localAddr, port);
+            server.Start();
             StartListener();
         }
 
@@ -29,40 +27,42 @@ namespace Deep_Jira_Server
                 while (true)
                 {
                     Console.WriteLine("Waiting for a connection...");
-                    HttpListenerContext context = webServer.GetContext();
+                    TcpClient client = server.AcceptTcpClient();
                     Console.WriteLine("Connected!");
                     Thread t = new Thread(new ParameterizedThreadStart(HandleDeivce));
-                    t.Start(context);
+                    t.Start(client);
                 }
             }
             catch (SocketException e)
             {
                 Console.WriteLine("SocketException: {0}", e);
-                webServer.Stop();
+                server.Stop();
             }
         }
 
         public void HandleDeivce(object obj)
         {
-            HttpListenerContext context = (HttpListenerContext)obj;
+            TcpClient client = (TcpClient)obj;
 
             try
             {
-                HttpListenerRequest request = context.Request;
-
-                HttpListenerResponse response = context.Response;
-
-                string responseString = "<HTML><Body> Hello world!</Body></HTML>";
-                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-
-                response.ContentLength64 = buffer.Length;
-                Stream output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-                output.Close();
+                byte[] bytes = new byte[1024];
+                Stream stream = client.GetStream();
+                HttpProcessor httpProcessor = new HttpProcessor();
+                HttpRequest request = httpProcessor.GetRequest(stream);
+                Console.WriteLine(request.Method);
+                //httpProcessor.WriteRequest();
+                //HttpRequest request = HttpRequest();
+                //dynamic req = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(bytes));//<object>(Encoding.UTF8.GetString(bytes),settings);
+                //Console.WriteLine(request.ToString());
+                //Console.WriteLine("sended request");
+                //client.Close();
+                //httpProcessor.WriteRequest((string)req.issue.key, (string)req.user.key, (string)req.issue.fields.customfield_10404[0]);
             }
             catch (Exception e)
             {
                 Console.WriteLine("Exception: {0}", e.ToString());
+                client.Close();
             }
         }
     }
